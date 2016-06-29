ea30aa94da9a0dba82805c1c8e3fad3ecc9ce270
ChuchaCuerera <ara_altver@hotmail.com>
2016-06-28 21:38:55 -0500

    fin de mi proyecto

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,36 @@
 class Item < ActiveRecord::Base
 
-	has_one :operation
+	has_one :operation, dependent: :destroy
+
+
+
+
+
+
+	#devuelva todas las piezas de inventario del primer operador registrado
+		scope :first_worker, -> {where(worker_id: Worker.first.id)}  
+
+
+	#Crear un scope que nos devuelva todas las piezas de inventario que no hayan sido utilizadas en alguna operación
+
+		scope :no_operation, -> {where(operation_id: nil)} 
+
+
+
+#callbacks
+after_destroy :write_to_record
+
+
+
+#methods
+#  "La pieza con serial (especificar Nº de
+# serie) ha sido borrada"
+protected
+
+def write_to_record
+	Record.create(message: "La pieza con serial #{serial} ha sido borrada")
+end 
+
+
+
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,4 +1,33 @@
 class Operation < ActiveRecord::Base
   belongs_to :worker
   belongs_to :item
+
+
+
+
+#devuelve las últimas 5 operaciones
+scope :last_five, -> {order(id: :desc).limit(5)}  
+
+#Crear un scope que nos devuelva todas las operaciones hechas el último día
+
+scope :last_day, -> {where("Date(created_at) = ?", Date.current)}  
+	
+
+
+#callbacks
+after_destroy :write_to_record
+
+
+
+#methods
+# "La operación con referencia a la pieza
+# (especificar Nº de serie) y al usuario (especificar id y nombre del usuario) ha sido borrada
+protected
+
+def write_to_record
+	Record.create(message: "La operación con referencia a la pieza #{serial} y al usuario #{worker.id.name} ha sido borrada")
+end 
+
+
+
 end
